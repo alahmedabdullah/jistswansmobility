d52f1184b45e9987e889c3fdae129b6a251c659a
fagnerpb <fagnerpb@56b5eb04-9792-11dd-9a07-5b26a99b1435>
2008-10-23 17:17:56 +0000

    simulaçao SBRC pronta

--- jist-swans-1.0.6/src/driver/run.bat
+++ jist-swans-1.0.6/src/driver/run.bat
@@ -4,10 +4,12 @@ cd amostra1
 mkdir waypoint0-20-1
 cd waypoint0-20-1
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ..
 mkdir waypoint1-20-1
 cd waypoint1-20-1
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ../..
 
 mkdir amostra2
@@ -15,10 +17,12 @@ cd amostra2
 mkdir waypoint0-20-2
 cd waypoint0-20-2
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ..
 mkdir waypoint1-20-2
 cd waypoint1-20-2
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ../..
 
 mkdir amostra3
@@ -26,10 +30,12 @@ cd amostra3
 mkdir waypoint0-20-3
 cd waypoint0-20-3
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ..
 mkdir waypoint1-20-3
 cd waypoint1-20-3
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ../..
 
 mkdir amostra4
@@ -37,10 +43,12 @@ cd amostra4
 mkdir waypoint0-20-4
 cd waypoint0-20-4
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ..
 mkdir waypoint1-20-4
 cd waypoint1-20-4
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ../..
 
 mkdir amostra5
@@ -48,10 +56,12 @@ cd amostra5
 mkdir waypoint0-20-5
 cd waypoint0-20-5
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ..
 mkdir waypoint1-20-5
 cd waypoint1-20-5
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ../..
 
 mkdir amostra6
@@ -59,45 +69,343 @@ cd amostra6
 mkdir waypoint0-20-6
 cd waypoint0-20-6
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ..
 mkdir waypoint1-20-6
 cd waypoint1-20-6
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ../..
 
+mkdir amostra7
+cd amostra7
 mkdir waypoint0-20-7
 cd waypoint0-20-7
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ..
 mkdir waypoint1-20-7
 cd waypoint1-20-7
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ../..
 
+
+mkdir amostra8
+cd amostra8
 mkdir waypoint0-20-8
 cd waypoint0-20-8
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ..
 mkdir waypoint1-20-8
 cd waypoint1-20-8
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ../..
 
+mkdir amostra9
+cd amostra9
 mkdir waypoint0-20-9
 cd waypoint0-20-9
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ..
 mkdir waypoint1-20-9
 cd waypoint1-20-9
 java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
 cd ../..
 
-#java jist.runtime.Main driver.aodvsim  -n 100 -f 500x300 -l uniform:0.02 -a random -m uniform_rect:0:20:10:5   -t 10,300,10 -x true
+mkdir amostra10
+cd amostra10
+mkdir amostra10
+cd amostra10
+mkdir waypoint0-20-10
+cd waypoint0-20-10
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-10
+cd waypoint1-20-10
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
 
 
-#java jist.runtime.Main driver.aodvsim  -n 100 -f 500x300 -l uniform:0.02 -a random -m nomande_rect_uniform:0:20:5000:10:50:0:5   -t 10,300,10 -x true
+mkdir amostra11
+cd amostra11
+mkdir waypoint0-20-11
+cd waypoint0-20-11
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-11
+cd waypoint1-20-11
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
 
-#java jist.runtime.Main driver.aodvsim  -n 100 -f 500x300 -l uniform:0.02 -a random -m gauss_markov:0:20:3:3.14:0.75:0.2   -t 10,300,10 -x true
+mkdir amostra12
+cd amostra12
+mkdir waypoint0-20-12
+cd waypoint0-20-12
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-12
+cd waypoint1-20-12
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra13
+cd amostra13
+mkdir waypoint0-20-13
+cd waypoint0-20-13
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-13
+cd waypoint1-20-13
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra14
+cd amostra14
+mkdir waypoint0-20-14
+cd waypoint0-20-14
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-14
+cd waypoint1-20-14
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra15
+cd amostra15
+mkdir waypoint0-20-15
+cd waypoint0-20-15
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-15
+cd waypoint1-20-15
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra16
+cd amostra16
+mkdir waypoint0-20-16
+cd waypoint0-20-16
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-16
+cd waypoint1-20-16
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra17
+cd amostra17
+mkdir waypoint0-20-17
+cd waypoint0-20-17
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-17
+cd waypoint1-20-17
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+
+mkdir amostra18
+cd amostra18
+mkdir waypoint0-20-18
+cd waypoint0-20-18
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-18
+cd waypoint1-20-18
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra19
+cd amostra19
+mkdir waypoint0-20-19
+cd waypoint0-20-19
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-19
+cd waypoint1-20-19
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra20
+cd amostra20
+mkdir amostra20
+cd amostra20
+mkdir waypoint0-20-20
+cd waypoint0-20-20
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-20
+cd waypoint1-20-20
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+
+mkdir amostra21
+cd amostra21
+mkdir waypoint0-20-21
+cd waypoint0-20-21
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-21
+cd waypoint1-20-21
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra22
+cd amostra22
+mkdir waypoint0-20-22
+cd waypoint0-20-22
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-22
+cd waypoint1-20-22
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra23
+cd amostra23
+mkdir waypoint0-20-23
+cd waypoint0-20-23
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-23
+cd waypoint1-20-23
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra24
+cd amostra24
+mkdir waypoint0-20-24
+cd waypoint0-20-24
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-24
+cd waypoint1-20-24
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra25
+cd amostra25
+mkdir waypoint0-20-25
+cd waypoint0-20-25
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-25
+cd waypoint1-20-25
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra6
+cd amostra6
+mkdir waypoint0-20-26
+cd waypoint0-20-26
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-26
+cd waypoint1-20-26
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra27
+cd amostra27
+mkdir waypoint0-20-27
+cd waypoint0-20-27
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-27
+cd waypoint1-20-27
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+
+mkdir amostra28
+cd amostra28
+mkdir waypoint0-20-28
+cd waypoint0-20-28
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-28
+cd waypoint1-20-28
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra29
+cd amostra29
+mkdir waypoint0-20-29
+cd waypoint0-20-29
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-29
+cd waypoint1-20-29
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+mkdir amostra30
+cd amostra30
+mkdir amostra30
+cd amostra30
+mkdir waypoint0-20-30
+cd waypoint0-20-30
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:0:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ..
+mkdir waypoint1-20-30
+cd waypoint1-20-30
+java jist.runtime.Main driver.aodvsim  -n 50 -f 1500x300 -l uniform:0.02 -a random -m waypoint:0:1:1:20   -t 10,900,10 -x true
+analizer_jist_swans aodvsim_output_jist_swans.nam
+cd ../..
+
+@java jist.runtime.Main driver.aodvsim  -n 100 -f 500x300 -l uniform:0.02 -a random -m uniform_rect:0:20:10:5   -t 10,300,10 -x true
+
+
+@java jist.runtime.Main driver.aodvsim  -n 100 -f 500x300 -l uniform:0.02 -a random -m nomande_rect_uniform:0:20:5000:10:50:0:5   -t 10,300,10 -x true
+
+@java jist.runtime.Main driver.aodvsim  -n 100 -f 500x300 -l uniform:0.02 -a random -m gauss_markov:0:20:3:3.14:0.75:0.2   -t 10,300,10 -x true
 
 
 
